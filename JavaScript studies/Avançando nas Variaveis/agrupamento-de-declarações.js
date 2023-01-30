bca0f186c966a04a692fd86e968b2f04cc2be33e
Gabriel-A-Ferretto <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2023-01-30 08:00:17 -0300

    Estudos de JS

---
+++ JavaScript studies/Avançando nas Variaveis/agrupamento-de-declarações.js
@@ -0,0 +1,13 @@
+// Agrupando declarações em variáveis
+let name = "Bilbo", age = 20, city = "Berlim";
+
+console.log("Name", name);
+console.log("Age", age);
+console.log("City", city);
+
+let job, food, car;
+job = "Programmer", food = "Burger-King", car = "Nissan 240SX";
+
+console.log("Job", job);
+console.log("Food", food);
+console.log("Car", car);
---
+++ JavaScript studies/Avançando nas Variaveis/declaration-assignment_var-let.js
@@ -0,0 +1,12 @@
+let x; // undefinde
+console.log("x", x);
+
+x = 10;
+console.log("x", x);
+console.log("x", typeof x);
+
+x = "A";
+console.log("x", x);
+
+let y = 20; // declaration assignment var / let
+console.log("y", y);
