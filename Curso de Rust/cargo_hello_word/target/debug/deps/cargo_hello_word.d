606a5f789bedc4f08d04df4ea6e4b7fca471ee07
Gabriel-A-Ferretto <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2024-02-16 22:21:11 -0300

    study

---
+++ Curso de Rust/cargo_hello_word/target/debug/deps/cargo_hello_word.d
@@ -0,0 +1,5 @@
+C:\Users\artes\Documents\GitHub\Studies\Curso de Rust\cargo_hello_word\target\debug\deps\cargo_hello_word.exe: src\main.rs
+
+C:\Users\artes\Documents\GitHub\Studies\Curso de Rust\cargo_hello_word\target\debug\deps\cargo_hello_word.d: src\main.rs
+
+src\main.rs:
