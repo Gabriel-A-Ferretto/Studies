252194d9bcf6d2591723d9ca7695f5a1254b805e
Gabriel-A-Ferretto <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2024-03-08 16:17:26 -0300

    Delete Curso de Rust directory

--- Curso de Rust/cargo_hello_word/target/debug/deps/cargo_hello_word-fe7d1a3334cadac0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\artes\Documents\GitHub\Studies\Curso de Rust\cargo_hello_word\target\debug\deps\libcargo_hello_word-fe7d1a3334cadac0.rmeta: src\main.rs
-
-C:\Users\artes\Documents\GitHub\Studies\Curso de Rust\cargo_hello_word\target\debug\deps\cargo_hello_word-fe7d1a3334cadac0.d: src\main.rs
-
-src\main.rs:
