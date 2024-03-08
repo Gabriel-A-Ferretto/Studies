d78a44903786ff2a40971fccc8c77bfdcf0b6333
Gabriel-A-Ferretto <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2024-03-08 16:18:23 -0300

    Delete C++/C++ Iniciante directory

--- C++/C++ Iniciante/Variaveis/var01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    int x = 7; 
-
-    std::cout << "O valor de x  é " << x << std:: endl;
-    // :: é chamado de operador de resolução de escopo
-    return 0;
-}
--- C++/C++ Iniciante/Variaveis/var03.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    float  y = -87.1;
-    std::cout << "O valor de y é: " << y << std:: endl;
-    return 0;
-}
